0c0497390f6815930e7a4ff6ff4ec1f86c39871f
SHYAM DADHANIYA <74183113+ShyamDadhaniya@users.noreply.github.com>
2022-07-06 18:36:39 +0530

    Add files via upload

---
+++ dashboard-react-app/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import StoreIcon from "@mui/icons-material/Store";
+import InsertChartIcon from "@mui/icons-material/InsertChart";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+
+const Sidebar = () => {
+  return (
+    <>
+      <div className="sidebar">
+        <div className="top">
+          <span className="logo">Shyam Dadhaniya</span>
+        </div>
+        <hr />
+        <div className="center">
+          <ul>
+            <li>
+              <DashboardIcon />
+              <span>Dashboard</span>
+            </li>
+            <li>
+              <PersonOutlineIcon />
+              <span>Users</span>
+            </li>
+            <li>
+              <StoreIcon />
+              <span>Products</span>
+            </li>
+            <li>
+              <CreditCardIcon />
+              <span>Orders</span>
+            </li>
+            <li>
+              <LocalShippingIcon />
+              <span>Delivery</span>
+            </li>
+            <li>
+              <InsertChartIcon />
+              <span>Stats</span>
+            </li>
+            <li>
+              <NotificationsNoneIcon />
+              <span>Notifications</span>
+            </li>
+            <li>
+                <SettingsSystemDaydreamOutlinedIcon />
+              <span>System Health</span>
+            </li>
+            <li>
+              <PsychologyOutlinedIcon />
+              <span>Logs</span>
+            </li>
+
+            <li>
+              <SettingsApplicationsIcon />
+              <span>Settings</span>
+            </li>
+            <li>
+                <AccountCircleOutlinedIcon/>
+              <span>Profile</span>
+            </li>
+            <li>
+                <ExitToAppIcon />
+              <span>Logout</span>
+            </li>
+          </ul>
+        </div>
+        <div className="bottom">color options</div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
